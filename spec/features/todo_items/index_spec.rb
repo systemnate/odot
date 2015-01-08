b6567c433af43117cfe13bdcdb2e640373c53afe
Nate Dalo <nate.dalo@yahoo.com>
2015-01-07 22:53:52 -0800

    add id attribute to remove css selection ambiguity

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -5,7 +5,7 @@ describe "Viewing todo items" do
 
   it "displays the title of the todo list" do
     visit_todo_list(todo_list)
-    within("h1") do
+    within("#title") do
       expect(page).to have_content(todo_list.title)
     end
   end    
